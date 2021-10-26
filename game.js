48e5ee9f86a1c7ce9db86f1bcda2e6f7cbba60e1
Shivaanee <89640504+Shivaanee@users.noreply.github.com>
2021-10-26 09:45:35 +0400

    default bear speed 100

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function Bear()
 {
-	this.dBear = document.getElementById("bearspeed").value;
+	this.dBear = 100;
 	this.htmlElement = document.getElementById("bear");
 	this.id = this.htmlElement.id;
 	this.x = this.htmlElement.offsetLeft;
